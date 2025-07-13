4e56165caec079635f566de120956048366cfc75
KOTA NIKHILL NANDAN <124546445+NikhillNandan@users.noreply.github.com>
2025-07-13 06:46:45 +0530

    Add files via upload

---
+++ Choice.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Choice {
+public static void main(String[] args){
+    int num=2;
+    switch(num){
+        case 1:System.out.println("MAGGI");
+        case 2:System.out.println("MILK");
+        break;
+        case 3:System.out.println("MONEY");
+        break;
+        default :System.out.println("nothing");
+    }
+}
+}
